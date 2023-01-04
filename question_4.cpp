ade8d266056a25b2a3c590acf23d6f6a6c8df9f2
patrickrpandrade <108357106+patrickrpandrade@users.noreply.github.com>
2023-01-04 01:19:58 -0300

    Create question_4.cpp

---
+++ question_4.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main ()
+{
+  string s = "acb";
+  string t = "abcde";
+  int k = 8;
+  int steps = 0;
+  int overplus = 0;
+  int size = 0;
+  bool empty = false;
+  if (s.length() <= t.length())
+  {
+     steps += (t.length() - s.length());
+     size = s.length();
+  }
+  else
+  {
+    steps += (s.length() - t.length());
+    size = t.length();
+  }
+  for (int i = 0; i < size; i++)
+  {
+     if (t[i] != s[i])
+	   {
+	     if (i == 0)
+	     {
+	        empty = true;
+	      }
+	   steps += 2 * (size - i);
+	   break;
+	   }
+   }
+  if (k == steps)
+  {
+     cout << "Yes";
+  }
+  else if (k > steps)
+  {
+    overplus = (k - steps);
+    if (empty)
+    {
+      cout << "Yes";
+    }
+    else if ((overplus % 2) == 0)
+    {
+      cout << "Yes";
+    }
+    else
+    {
+      cout << "No";
+    }
+  }
+  else
+  {
+      cout << "No";
+  }
+  return 0;
+}
